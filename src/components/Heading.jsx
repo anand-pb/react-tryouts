33123b3ec3ac0c93e23da10719683d1e77e55074
anand_pb <apb.softdev@gmail.com>
2023-03-18 17:54:02 +0530

    refactored the files into components folder

---
+++ src/components/Heading.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+var mcustomStyle = {
+    color: "lightblue"
+}
+  
+var acustomStyle = {
+    color: "lightgreen"
+}
+  
+var ecustomStyle = {
+    color: "darkgray"
+}
+  
+var colorobj = {}
+var greeting = ""
+  
+var randnum = Math.floor(Math.random() * 24);
+var today = new Date(1, 1, 1, randnum);
+  
+var hour = today.getHours();
+  
+if (hour >= 0 && hour < 12) {
+  colorobj = mcustomStyle;
+  greeting = "Good morning"
+} else if (hour >= 12 && hour < 18) {
+  colorobj = acustomStyle;
+  greeting = "Good afternoon"
+} else {
+  colorobj = ecustomStyle;
+  greeting = "Good evening"
+}
+
+function Heading() {
+    return (
+        <div>
+            <h1 className="heading" style={colorobj}>{greeting}</h1>
+            <p>{randnum} : 00</p>
+        </div>
+    );
+}
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-var mcustomStyle = {
-  color: "lightblue"
-}
-
-var acustomStyle = {
-  color: "lightgreen"
-}
-
-var ecustomStyle = {
-  color: "darkgray"
-}
-
-var colorobj = {}
-var greeting = ""
-
-var randnum = Math.floor(Math.random() * 24);
-var today = new Date(1, 1, 1, randnum);
-
-var hour = today.getHours();
-
-if (hour >= 0 && hour < 12) {
-  colorobj = mcustomStyle;
-  greeting = "Good morning"
-} else if (hour >= 12 && hour < 18) {
-  colorobj = acustomStyle;
-  greeting = "Good afternoon"
-} else {
-  colorobj = ecustomStyle;
-  greeting = "Good evening"
-}
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <h1 className="heading" style={colorobj}>{greeting}</h1>
-    <p>{randnum} : 00</p>
-  </div>
+  <App />
 );
