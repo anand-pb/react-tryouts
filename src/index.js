125d3307eeb463ecfd5d8b0f2f202d7bf236ad17
anand_pb <apb.softdev@gmail.com>
2023-03-18 18:55:21 +0530

    refactored arithmetic functions into a separate module

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import {add, subtract, multiply, dividedby} from './calc';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <div>
+    <ul>
+      <li>{add(1, 2)}</li>
+      <li>{subtract(2, 3)}</li>
+      <li>{multiply(7, 2)}</li>
+      <li>{dividedby(5, 2)}</li>
+    </ul>
+  </div>
+    // <App />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
