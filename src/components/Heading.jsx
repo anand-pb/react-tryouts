94535661c34c84604961ccdfabc73ea62206d470
anand_pb <apb.softdev@gmail.com>
2023-03-27 23:10:27 +0530

    27032023

---
+++ src/components/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading() {
+    return <h1>My Mentors Through Crisis</h1>;
+}
+
+export default Heading;
---
+++ src/components/List.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function List() {
+    return <ul><li>William Thomson</li><li>Michael Faraday</li><li>John Von Neumann</li></ul>;
+}
+
+export default List;
