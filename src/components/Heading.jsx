5a9877a0ae3a03f995979d8d424f295469a7ea37
anand_pb <apb.softdev@gmail.com>
2023-03-27 23:07:59 +0530

    27032023

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 
-var mcustomStyle = {
-    color: "lightblue"
-}
-  
-var acustomStyle = {
-    color: "lightgreen"
-}
-  
-var ecustomStyle = {
-    color: "darkgray"
-}
-  
-var colorobj = {}
-var greeting = ""
-  
-var randnum = Math.floor(Math.random() * 24);
-var today = new Date(1, 1, 1, randnum);
-  
-var hour = today.getHours();
-  
-if (hour >= 0 && hour < 12) {
-  colorobj = mcustomStyle;
-  greeting = "Good morning"
-} else if (hour >= 12 && hour < 18) {
-  colorobj = acustomStyle;
-  greeting = "Good afternoon"
-} else {
-  colorobj = ecustomStyle;
-  greeting = "Good evening"
-}
-
 function Heading() {
+    var mcustomStyle = {
+        color: "lightblue"
+    }
+      
+    var acustomStyle = {
+        color: "lightgreen"
+    }
+      
+    var ecustomStyle = {
+        color: "darkgray"
+    }
+      
+    var colorobj = {}
+    var greeting = ""
+      
+    var randnum = Math.floor(Math.random() * 24);
+    var today = new Date(1, 1, 1, randnum);
+      
+    var hour = today.getHours();
+      
+    if (hour >= 0 && hour < 12) {
+      colorobj = mcustomStyle;
+      greeting = "Good morning"
+    } else if (hour >= 12 && hour < 18) {
+      colorobj = acustomStyle;
+      greeting = "Good afternoon"
+    } else {
+      colorobj = ecustomStyle;
+      greeting = "Good evening"
+    }
+
     return (
         <div>
             <h1 className="heading" style={colorobj}>{greeting}</h1>
