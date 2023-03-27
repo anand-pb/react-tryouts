eaddf535e849b365d8ac85234b5cf2914c0dc517
anand_pb <apb.softdev@gmail.com>
2023-03-27 23:48:12 +0530

    tryout19reactjs23032023

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +9,20 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const citrus = ["Lime", "Lemon", "Orange"];
+// const fruits = ["Apple", "Banana", "Coconut", ...citrus];
+
+// console.log(fruits);
+
+// const fullName = {
+//   fName: "Johann",
+//   lName: "Kepler"
+// };
+
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   username: "jkepler"
+// };
+
+// console.log(user);
