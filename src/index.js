aec4c9d98e8eaff249243789c713c1dbd106b588
anand_pb <apb.softdev@gmail.com>
2023-03-27 23:25:09 +0530

    tryout7reactjs11032023

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {add, subtract, multiply, dividedby} from './calc';
+// import * as Calculator from './calc';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     <ul>
-      <li>{add(1, 2)}</li>
-      <li>{subtract(2, 3)}</li>
-      <li>{multiply(7, 2)}</li>
-      <li>{dividedby(5, 2)}</li>
+      <li>{add(1, 2)}</li>	//Calculator.add()
+      <li>{subtract(2, 3)}</li>	 //Calculator.subtract()
+      <li>{multiply(7, 2)}</li>  //Calculator.multiply()
+      <li>{dividedby(5, 2)}</li>  //Calculator.dividedby()
     </ul>
   </div>
     // <App />
