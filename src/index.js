d89c1225302bad46216fb3681c9e3ea7c3907c7c
anand_pb <apb.softdev@gmail.com>
2023-03-27 23:39:50 +0530

    tryout14reactjs18032023

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import cars from './details';
+
+const [honda, tesla] = cars;
+
+// var hondaTopSpeed = honda.speedStats.topSpeed; 
+// var hondaTopColour = honda.coloursByPopularity[0];
+
+// var teslaTopSpeed = tesla.speedStats.topSpeed; 
+// var teslaTopColour = tesla.coloursByPopularity[0];
+
+const {coloursByPopularity : [hondaTopColour], speedStats : {topSpeed : hondaTopSpeed }} = honda;
+const {coloursByPopularity : [teslaTopColour], speedStats : {topSpeed : teslaTopSpeed }} = tesla;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <table>
+        <tr>
+          <th>Brand</th>
+          <th>Top Speed</th>
+          <th>Top Color</th>
+        </tr>
+        <tr>
+          <td>{tesla.model}</td>
+          <td>{teslaTopSpeed}</td>
+          <td>{teslaTopColour}</td>
+        </tr>
+        <tr>
+          <td>{honda.model}</td>
+          <td>{hondaTopSpeed}</td>
+          <td>{hondaTopColour}</td>
+        </tr>
+      </table>
+    </div>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
